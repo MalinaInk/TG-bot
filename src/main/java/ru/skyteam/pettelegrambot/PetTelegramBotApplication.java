f5e7bd6fe4b2acc5f962e8d37ec0ab9a0219bc83
Glebova <e.tulzakova@mail.ru>
2023-04-13 15:28:10 +0400

    Initial commit

---
+++ src/main/java/ru/skyteam/pettelegrambot/PetTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.skyteam.pettelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skyteam/pettelegrambot/PetTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skyteam.pettelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
